80db592723cb896e7e080791beafde87f883033c
yuetsh <517252939@qq.com>
2025-03-03 10:23:10 +0800

    update

--- account/models.py
+++ account/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 
+
+class RoleChoices(models.TextChoices):
+    SUPER = "sup    er", "超级管理员"
+    ADMIN = "admin", "管理员"
+    NORMAL = "normal", "普通"
+
+
 class User(AbstractUser):
-    pass
+    role = models.CharField(
+        max_length=20,
+        choices=RoleChoices.choices,
+        default=RoleChoices.NORMAL,
+    )
