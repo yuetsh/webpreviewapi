f5de2d149ca05e511587fcd3ccf04818cbe70294
yuetsh <517252939@qq.com>
2025-03-04 23:59:08 +0800

    update

--- account/admin.py
+++ account/admin.py
@@ -9,9 +9,16 @@ admin.site.site_title = "前端开发网站管理"
 
 admin.site.unregister(Group)
 
+
 @admin.register(User)
 class AdminAccount(UserAdmin):
     fieldsets = ((None, {"fields": ("username", "password", "role", "is_active")}),)
+    add_fieldsets = (
+        (
+            None,
+            {"fields": ("username", "password1", "password2", "role")},
+        ),
+    )
     list_display = ("username", "role", "is_active")
     list_filter = ("role", "is_active")
     search_fields = ("username",)
