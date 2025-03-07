5b3645c68b49369829ef03d0502086f00f7eabba
yuetsh <517252939@qq.com>
2025-03-07 14:05:29 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python manage.py migrate --noinput
-exec gunicorn api.wsgi -k uvicorn.workers.UvicornWorker --workers 5 --threads 4
+exec gunicorn api.wsgi --bind :8000 -k uvicorn.workers.UvicornWorker --workers 5 --threads 4
