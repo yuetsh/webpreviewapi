d23e0d65fb2d845aa0cf00e61de8ae77853852fa
yuetsh <517252939@qq.com>
2025-03-09 20:05:27 +0800

    default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python manage.py migrate --noinput
-exec gunicorn --bind 0.0.0.0:8000 api.asgi:application -k gevent --workers 5
+exec gunicorn --bind 0.0.0.0:8000 api.asgi:application -k uvicorn.workers.UvicornWorker
