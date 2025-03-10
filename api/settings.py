332d685e8b41706eac52499f5b76f6d21e3c2aaa
yuetsh <517252939@qq.com>
2025-03-10 13:48:47 +0800

    add cache

--- api/settings.py
+++ api/settings.py
@@ -106,10 +106,18 @@ PROD_DATABASES = {
     },
 }
 
+PROD_CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "webpreview_api_cache",
+    }
+}
+
 if DEBUG:
     DATABASES = DEV_DATABASES
 else:
     DATABASES = PROD_DATABASES
+    CACHES = PROD_CACHES
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
