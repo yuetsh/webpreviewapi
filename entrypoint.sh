c4ff0515092518e1b29ddb31f2a3201b5b0fe181
yuetsh <517252939@qq.com>
2025-05-10 08:40:22 +0800

    remove dbpool

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# 等待 Redis 和 PgBouncer 准备就绪
 sleep 5
 
 # 执行数据库迁移
@@ -11,9 +10,4 @@ exec gunicorn api.asgi:application \
     --bind 0.0.0.0:8000 \
     --worker-class uvicorn.workers.UvicornWorker \
     --workers 4 \
-    --threads 2 \
-    --timeout 120 \
-    --keep-alive 65 \
-    --max-requests 1000 \
-    --max-requests-jitter 50 \
-    --log-level info
+    --threads 2
