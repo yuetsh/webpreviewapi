7990b886c843a4f0525dff8325b9ccc5758ba1b2
yuetsh <517252939@qq.com>
2026-03-09 09:47:40 +0800

    fix

--- api/asgi.py
+++ api/asgi.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 from prompt.url import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 application = ProtocolTypeRouter(
     {
